68549df44d5d6833054426bee27f03cc01738145
hoatrong18 <hoatrong2008td@gmail.com>
2026-01-16 22:16:32 +0700

    update website

--- developerFolio-master/src/portfolio.js
+++ developerFolio-master/src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A high school student specializing in Information Technology, passionate about AI and software development, with experience in data engineering, competitive programming, and building real-world tech projects."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1DbKmTx_IwJU0mSjPAISnAyy543bQsGhy/view", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1SATIacVA2JJuHKsvGXKFMvgsqUBZorfS/view", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
